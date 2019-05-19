c2ca96d87f4a8570f8c1f7bc172166ae68729023
Geovane Schmitz <geovane963hyg@gmail.com>
2019-05-19 16:43:31 -0300

    Primeira versão

---
+++ index.js
@@ -0,0 +1,223 @@
+const Sigaa = require("sigga-api")
+
+const https = require('https');
+const fs = require('fs');
+const uuid = require('uuid/v4');
+const path = require('path');
+const querystring = require('querystring');
+const Telegram = require('telegraf/telegram')
+const sigaa = new Sigaa();
+
+const credentials = require("./credentials")
+
+let storeDataFilename = "./data.json";
+let data = require(storeDataFilename)
+let BaseDestiny = "/tmp"
+let token;
+
+const telegram = new Telegram(credentials.token)
+
+let getUpdate = async () =>{
+  await sigaa.account
+  .login(credentials.username, credentials.password) // login
+  .then(res => {
+    /* res = {
+      status:'LOGGED',
+      userType:'STUDENT',
+      token: random string
+    }
+    */
+    if (res.userType === 'STUDENT') {
+      token = res.token; // this stores access token
+      return sigaa.classStudent.getClasses(res.token); // this return a array with all classes
+    } else {
+      throw 'user is not a student'
+    }
+  })
+  .then(classes => {
+    return new Promise((resolve, reject) => {
+      classTopics(classes)
+        .then(() => resolve(classes))
+    })
+  })
+  .then((classes) => {
+    return ClassNews(classes) // logoff afeter finished downloads 
+  })
+  .then(() => {
+    return sigaa.account.logoff(token) // logoff afeter finished downloads 
+  })
+  .catch(data => {
+    console.log(data);
+  });
+}
+
+
+let saveData = () => {
+  fs.writeFile(storeDataFilename, JSON.stringify(data), function (err) {
+    if (err) throw err;
+  }
+  );
+}
+
+
+async function ClassNews(classes) {
+  for (let studentClass of classes) { //for each class
+    console.log(studentClass.name)
+    var news = await sigaa.classStudent.getNewsIndex(
+      studentClass.id,
+      token
+    ); //this lists all news
+    if (!data.news[studentClass.name]) data.news[studentClass.name] = []
+
+    let dataNewsString = data.news[studentClass.name].map(JSON.stringify)
+
+    let newNews = news.filter(news => {
+      let cloneNews = JSON.parse(JSON.stringify(news))
+      delete cloneNews.newsId.postOptions["javax.faces.ViewState"]
+      return dataNewsString.indexOf(JSON.stringify(cloneNews)) === -1
+    })
+
+    data.news[studentClass.name] = news.map(news => {
+      let cloneNews = JSON.parse(JSON.stringify(news))
+      delete cloneNews.newsId.postOptions["javax.faces.ViewState"]
+      return cloneNews
+    });
+    saveData()
+    for (let news of newNews) { //for each news
+      let fullNews = await sigaa.classStudent.getNews(news.newsId, token)
+      let date = removeYear(escapeMsg(fullNews.date))
+      let msg = `${escapeMsg(studentClass.name)}\n<b>${escapeMsg(fullNews.name)}</b>\n${date}\n${escapeMsg(fullNews.content)}`
+
+      await telegram.sendMessage(credentials.chatId,
+        msg, { parse_mode: "html" })
+
+    }
+  }
+}
+
+
+async function classTopics(classes) {
+  for (let studentClass of classes) { //for each class
+    console.log(studentClass.name)
+    var topics = await sigaa.classStudent.getTopics(
+      studentClass.id,
+      token
+    ); //this lists all topics
+    if (!data.topics[studentClass.name]) data.topics[studentClass.name] = []
+
+    let dataTopicsString = data.topics[studentClass.name].map(JSON.stringify)
+    let newTopics = topics.filter(topic => {
+      let cloneTopic = JSON.parse(JSON.stringify(topic))
+      if (cloneTopic.attachments) {
+        for (let i = 0; i < cloneTopic.attachments.length; i++) {
+          delete cloneTopic.attachments[i].form.postOptions["javax.faces.ViewState"]
+        }
+      }
+      return dataTopicsString.indexOf(JSON.stringify(cloneTopic)) === -1
+    })
+    data.topics[studentClass.name] = topics.map(topic => {
+      let cloneTopic = JSON.parse(JSON.stringify(topic))
+
+      if (cloneTopic.attachments) {
+        for (let i = 0; i < cloneTopic.attachments.length; i++) {
+          delete cloneTopic.attachments[i].form.postOptions["javax.faces.ViewState"]
+        }
+      }
+      return cloneTopic
+    });
+    saveData()
+
+    for (let topic of newTopics) { //for each topic
+      let date = topic.startDate === topic.endDate ? removeYear(topic.startDate) : removeYear(topic.startDate) + " - " + removeYear(topic.endDate);
+
+      let msg = `${escapeMsg(studentClass.name)}\n<b>${escapeMsg(topic.name)}</b>\n${escapeMsg(date)}\n${escapeMsg(topic.contentText)}`
+
+      await telegram.sendMessage(credentials.chatId,
+        msg, { parse_mode: "html" })
+      for (let attachment of topic.attachments) {
+        if (attachment.type == 'file') {
+          let filepath = await downloadFile(studentClass, attachment);
+          let fileExtension = filepath.slice(-3)
+          let photoExtension = ['jpg', 'png', 'gif']
+
+          if (photoExtension.indexOf(fileExtension) > -1) {
+            await telegram.sendPhoto(credentials.chatId, {
+              source: filepath
+            })
+          } else {
+            await telegram.sendDocument(credentials.chatId, {
+              source: filepath
+            })
+          }
+        }
+      }
+    }
+  }
+}
+let removeYear = (date) => {
+  return date.slice(0, date.lastIndexOf("/"))
+
+}
+let escapeMsg = (msg) => {
+  return msg.replace(/\<p\>|'\n'|<br\/>|<br>|\t/gm, '\n').replace(
+    /<script([\S\s]*?)>([\S\s]*?)<\/script>|&nbsp;|<style([\S\s]*?)style>|<([\S\s]*?)>|<[^>]+>| +(?= )|\t/gm,
+    '')
+}
+async function downloadFile(studentClass, attachment) {
+  return await new Promise((resolve, reject) => {
+
+    fs.mkdir(path.join(BaseDestiny, studentClass.name), err => { // this creates path with class name
+      if (err && err.code != 'EEXIST')
+        throw 'up';
+
+      let tmpFileDestiny = path.join(BaseDestiny, uuid());
+      let file = fs.createWriteStream(tmpFileDestiny);
+
+      let link = new URL(attachment.form.action);
+
+      //http options
+      const options = {
+        hostname: link.hostname,
+        port: 443,
+        path: link.pathname + link.search,
+        method: 'POST',
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Cookie': token
+        },
+      };
+
+      // this converts post parameters to string
+      let postOptionsString = querystring.stringify(attachment.form.postOptions);
+      // this inserts post parameters length to  header http
+      options.headers['Content-Length'] = Buffer.byteLength(postOptionsString);
+
+      // makes request
+      var request = https.request(options, (response) => {
+        let filename = response.headers['content-disposition'].replace(/([\S\s]*?)filename=\"/gm, '').slice(0, -1);
+        let filepath = path.join(BaseDestiny, filename)
+        response.pipe(file); //save to file
+
+        file.on('finish', () => {
+          fs.rename(tmpFileDestiny, filepath, (err) => {
+            if (err) throw err;
+          })
+          file.close(resolve(filepath)); // close() is async, call resolve after close completes.
+        });
+      }).on('error', (err) => {
+        fs.unlink(tmpFileDestiny); // Delete the file async.
+        reject(false);
+      });
+      request.write(postOptionsString); //send post parameters
+      request.end();
+    });
+  });
+}
+
+require("http")
+  .createServer(async (req, res) => {
+    res.statusCode = 200; res.write("ok");
+    res.end();
+    await getUpdate();
+  }).listen(3000, () => console.log("Now listening on port 3000"));
